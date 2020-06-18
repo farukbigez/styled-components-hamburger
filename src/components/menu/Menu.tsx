3f65d59ff56cb990ce422de43144fb9577a7282a
bigez <farukbigez@hotmail.com>
2020-06-18 23:23:18 +0300

    ref={node} removed

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { StyledMenu } from './Menu.styled'
 
-interface Props {
-   open: boolean
+export interface Props {
+  open: boolean
 }
+
 
 const Menu: React.FC<Props> = ({ open }) => {
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,12 @@ import { Burger, Menu } from './components'
 
 const App: React.FC = () => {
   const [open, setOpen] = React.useState(false)
-  const node = React.useRef(null)
 
   return (
     <>
-      <div ref={node} >
-        <Burger open={open} setOpen={setOpen} />
-        <Menu open={open} />
-        </div>
-      </>
+      <Burger open={open} setOpen={setOpen} />
+      <Menu open={open} />
+    </>
   )
 
 }
