735f9bf06f1dc971bb16273110f640d5518be11b
bigez <farukbigez@hotmail.com>
2020-06-22 16:50:49 +0300

    V1.1

--- src/components/burger/Burger.styled.ts
+++ src/components/burger/Burger.styled.ts
@@ -5,45 +5,39 @@ interface Props {
 }
 
 export const StyledBurger = styled.button<Props>`
-  position: absolute;
-  top: 5%;
-  left: 2rem;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
   width: 2rem;
   height: 2rem;
-  background: transparent;
-  border: none;
-  cursor: pointer;
-  padding: 0;
-  z-index: 10;
+  position: fixed;
+  top: 15px;
+  right: 20px;
+  z-index: 20;
+  display: none;
 
-  &:focus {
-    outline: none;
+  @media (max-width: 768px) {
+    display: flex;
+    justify-content: space-around;
+    flex-flow: column nowrap;
   }
 
   div {
     width: 2rem;
     height: 0.25rem;
-    background: ${({ theme, open }) =>
-      open ? theme.primaryDark : theme.primaryLight};
+    background-color: ${({ open }) => open ? '#ccc' : '#333'};
     border-radius: 10px;
-    transition: all 0.3s linear;
-    position: relative;
     transform-origin: 1px;
+    transition: all 0.3s linear;
 
-    :first-child {
-      transform: ${({ open }) => (open ? 'rotate(45deg)' : 'rotate(0)')};
+    &:nth-child(1) {
+      transform: ${({ open }) => open ? 'rotate(45deg)' : 'rotate(0)'};
     }
 
-    :nth-child(2) {
-      opacity: ${({ open }) => (open ? '0' : '1')};
-      transform: ${({ open }) => (open ? 'translateX(20px)' : 'translateX(0)')};
+    &:nth-child(2) {
+      transform: ${({ open }) => open ? 'translateX(100%)' : 'translateX(0)'};
+      opacity: ${({ open }) => open ? 0 : 1};
     }
 
-    :nth-child(3) {
-      transform: ${({ open }) => (open ? 'rotate(-45deg)' : 'rotate(0)')};
+    &:nth-child(3) {
+      transform: ${({ open }) => open ? 'rotate(-45deg)' : 'rotate(0)'};
     }
   }
-`
+`;
